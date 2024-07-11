c00b6757990061094c1f70ed0908a2ecb7df8810
fparuce <felipe_costacurta@hotmail.com>
2024-07-10 22:50:27 -0300

    Initial

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,79 @@
+<template>
+  <main class="conteudo-principal">
+    <SuaLista :ingredientes="ingredientes" />
+    <KeepAlive include="SelecionarIngredientes">
+      <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'" 
+        @adicionar-ingrediente="adicionarIngrediente"
+        @remover-ingrediente="removerIngrediente" 
+        @buscar-receitas="navegar('MostrarReceitas')" 
+      />
+      <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'"
+      :ingredientes="ingredientes"
+        @editar-receitas="navegar('SelecionarIngredientes')" 
+      />
+    </KeepAlive>
+  </main>
+</template>
+
+<script lang="ts">
+import SelecionarIngredientes from "./SelecionarIngredientes.vue";
+import SuaLista from "./SuaLista.vue";
+import Tag from "./Tag.vue";
+import MostrarReceitas from "./MostrarReceitas.vue";
+
+type Pagina = 'SelecionarIngredientes' | 'MostrarReceitas';
+
+export default {
+  components: {
+    SelecionarIngredientes,
+    SuaLista,
+    Tag,
+    MostrarReceitas
+  },
+  data() {
+    return {
+      ingredientes: [] as string[],
+      conteudo: 'SelecionarIngredientes' as Pagina
+    };
+  },
+  methods: {
+    adicionarIngrediente(ingrediente: string) {
+      this.ingredientes.push(ingrediente)
+    },
+    removerIngrediente(ingrediente: string) {
+      this.ingredientes = this.ingredientes.filter(iLista => ingrediente !== iLista);
+    },
+    navegar(pagina: Pagina) {
+      this.conteudo = pagina;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.conteudo-principal {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
